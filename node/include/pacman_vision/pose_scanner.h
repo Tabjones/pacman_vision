6df2aa32d56bfc45cf22b4b241ffa89f54a7320e
Federico Spinelli <fspinelli@gmail.com>
2015-09-28 18:21:12 +0200

    started pose scanner module

---
+++ node/include/pacman_vision/pose_scanner.h
@@ -0,0 +1,74 @@
+#ifndef _INCL_POSE_SCANNER
+#define _INCL_POSE_SCANNER
+
+#include <pacman_vision/config.h>
+//Utility
+#include <pacman_vision/utility.h>
+//PCL
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/eigen.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_plane.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/visualization/pcl_visualizer.h>
+// ROS generated headers
+#include <turn_table_interface/setPos.h>
+#include <turn_table_interface/getPos.h>
+#include <pacman_vision_comm/acquire.h>
+//general utilities
+#include <ctime>
+#include <algorithm>
+#include <boost/filesystem/path.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+//Storage
+#include <pacman_vision/storage.h>
+
+class VisionNode;
+
+class PoseScanner
+{
+  friend class VisionNode;
+
+  public:
+    PoseScanner(ros::NodeHandle &n, boost::shared_ptr<Storage> &stor);
+    ~PoseScanner();
+    //Eigen alignment
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  private:
+    ros::NodeHandle nh;
+    boost::shared_ptr<ros::CallbackQueue> queue_ptr;
+    boost::shared_ptr<Storage> storage;
+    //Service Server
+    ros::ServiceServer srv_acquire;
+
+    //method to move turn table
+    bool set_turn_table_pos(float pos);
+    //method to read turn table position
+    float get_turn_table_pos();
+
+    //table transform
+    boost::shared_ptr<Eigen::Matrix4f> table_transform;
+
+    //Save location
+    boost::filesystem::path work_dir;
+
+    //Scene processed
+    PC::Ptr scene;
+
+    //table pass in degrees
+    int table_pass;
+
+    //acquire service callback
+    bool cb_acquire(pacman_vision_comm::acquire::Request& req, pacman_vision_comm::acquire::Response& res);
+
+    //custom spin method
+    void spin_once();
+};
+#endif
--- node/src/pose_scanner.cpp
+++ node/src/pose_scanner.cpp
@@ -1,10 +1,36 @@
-#include <vcg/complex/complex.h>
+#include <pacman_vision/pose_scanner.h>
 
-class MyVertex; class MyEdge; class MyFace;
-struct MyUsedTypes : public vcg::UsedTypes<vcg::Use<MyVertex>   ::AsVertexType,
-                                           vcg::Use<MyEdge>     ::AsEdgeType,
-                                           vcg::Use<MyFace>     ::AsFaceType>{};
-class MyVertex  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
-class MyFace    : public vcg::Face<   MyUsedTypes, vcg::face::FFAdj,  vcg::face::VertexRef, vcg::face::BitFlags > {};
-class MyEdge    : public vcg::Edge<   MyUsedTypes> {};
-class MyMesh    : public vcg::tri::TriMesh< std::vector<MyVertex>, std::vector<MyFace> , std::vector<MyEdge>  > {};
+PoseScanner::PoseScanner(ros::NodeHandle &n, boost::shared_ptr<Storage> &stor)
+{
+  this->scene.reset(new PC);
+  this->nh = ros::NodeHandle(n, "pose_scanner");
+  this->queue_ptr.reset(new ros::CallbackQueue);
+  this->nh.setCallbackQueue(&(*this->queue_ptr));
+  this->storage = stor;
+  this->srv_acquire = nh.advertiseService("acquire", &PoseScanner::cb_acquire, this);
+  nh.param<int>("/pacman_vision/table_pass", table_pass, 10);
+  //TODO
+  std::string work_dir_s;
+  nh.param<std::string>("/pacman_vision/work_dir", work_dir_s, "~/PoseScanner");
+  work_dir = work_dir_s;
+  boost::posix_time::ptime timestamp(boost::posix_time::second_clock::local_time());
+  work_dir /= boost::filesystem::path()
+}
+
+bool PoseScanner::cb_acquire(pacman_vision_comm::acquire::Request& req, pacman_vision_comm::acquire::Response& res)
+{
+}
+
+bool PoseScanner::set_turn_table_pos(float pos)
+{
+}
+
+float PoseScanner::get_turn_table_pos()
+{
+}
+
+void PoseScanner::spin_once()
+{
+  //process this module callbacks
+  this->queue_ptr->callAvailable(ros::WallDuration(0));
+}
