3f711d12b6fc8e9c19d183542fe10ff07afa39fc
Federico Spinelli <fspinelli@gmail.com>
2015-11-29 01:07:03 +0100

    Further develop of base_ros_node_helpers, still wip

--- node/include/pacman_vision/base_ros_node_helpers.hpp
+++ node/include/pacman_vision/base_ros_node_helpers.hpp
@@ -1,7 +1,6 @@
 #ifndef _INCL_BASE_ROS_NODE_HELPERS_HPP_
 #define _INCL_BASE_ROS_NODE_HELPERS_HPP_
-
-//this header is hpp cause it also contains some implementations with templates
+//this header is hpp cause it also contains implementations
 
 #include <ros/ros.h>
 #include <ros/console.h>
@@ -11,57 +10,31 @@
 #include <ros/callback_queue_interface.h>
 #include <pacman_vision/common.h>
 #include <pacman_vision/storage.h>
+#include <unordered_map>
 
-// TODO: Add container of ModuleDriver masternode can inherit.(tabjones on Friday 27/11/2015)
-// TODO: Think of using driver without template, i.e. pointer to Module base (tabjones on Friday 27/11/2015)
-///Single Driver class
-template <typename TMOD>
-class ModuleDriver
-{
+///common members between master and modules
+class BaseCommon{
     public:
-        ModuleDriver():running(false){}
-        virtual ~ModuleDriver(){}
-
-        //spawn a thread "driving" the module
-        void spawn(TMOD &mod)
-        {
-            //subsequent calls to spawn are prohibited
-            if(!running){
-                module.reset(&mod);
-                worker = std::thread(module->spin());
-                running = true;
-            }
-            ROS_WARN("[%s]\tTried to spawn %s, but it is already spawned and running.",__func__,mod.string().c_str());
-        }
-        //kill a running module
-        void kill()
-        {
-            module->nh.shutdown();
-            worker.join();
-            module.reset();
-            running = false;
-        }
-        bool isRunning()
-        {
-            return (running);
-        }
-        typedef std::shared_ptr<ModuleDriver<TMOD>> Ptr;
+        virtual ~BaseCommon()=default;
     protected:
-        std::shared_ptr<TMOD> module;
-        std::thread worker;
-        bool running;
-}
-
-///Base class modules should inherit
-class Module
+        BaseCommon()=default;
+        ros::NodeHandle nh;
+        ros::Rate spin_rate;
+        std::string name_space;
+        std::shared_ptr<Storage> storage;
+};
+///Base class module, should be inherited by modules
+class Module : public BaseCommon
 {
-    friend class ModuleDriver; //module driver could mess with this module
     public:
+        ///empy module creation is not allowed
+        Module()=delete;
         //ctor with node handle of masternode, namespace, pointer to storage and desired rate
-        Module(const ros::NodeHandle &n, const std::string ns, std::shared_ptr<Storage> &stor, const ros::Rate rate):
+        Module(const ros::NodeHandle &n, const std::string ns, std::shared_ptr<Storage> &stor, const ros::Rate &rate):
             spin_rate(rate), storage(stor)
         {
             this->nh = ros::NodeHandle (n, ns);
+            this->name_space = ns;
             this->queue_ptr.reset(new ros::CallbackQueue);
             this->nh.setCallbackQueue(&(*queue_ptr));
         }
@@ -80,28 +53,92 @@ class Module
                 this->spinOnce();
                 spin_rate.sleep();
             }
-
+        }
+        virtual inline std::string getMyName() const
+        {
+            //Module identifier
+            return ns;
+        }
+        virtual inline Module::Ptr getMyPtr() const =0;
+        typedef std::shared_ptr<Module> Ptr
+    protected:
+        std::unique_ptr<ros::CallbackQueue> queue_ptr;
+};
+///Single Driver class, this makes a module live or die
+class ModuleDriver
+{
+    public:
+        ModuleDriver():running(false){}
+        virtual ~ModuleDriver()=default;
+        //spawn a thread "driving" the module
+        void spawn(Module:Ptr &mod)
+        {
+            //subsequent calls to spawn are prohibited
+            if(!running){
+                module = mod;
+                name = module->getMyName();
+                worker = std::thread(module->spin());
+                running = true;
+                return;
+            }
+            ROS_WARN("[%s]\tTried to spawn %s, but it is already spawned and running.",__func__,module->getMyName().c_str());
+        }
+        //kill a running module
+        void kill()
+        {
+            if (running){
+                module->nh.shutdown();
+                worker.join();
+                module.reset();
+                running = false;
+                return;
+            }
+            ROS_WARN("[%s]\tTried to kill %s, but it is not running.",__func__,module->getMyName().c_str());
+        }
+        inline bool isRunning() const
+        {
+            return (running);
+        }
+        inline auto getModule() const //this requires c++14 ..!
+        {
+            //Derived classes from Module are FORCED to implement getMyPtr, so
+            //with function you actually get a shared_ptr of Derived type.
+            return (module->getMyPtr());
+        }
+        typedef std::shared_ptr<ModuleDriver> Ptr;
+    protected:
+        Module::Ptr module;
+        std::thread worker;
+        bool running;
+        std::string name;
+};
+//Container of ModuleDriver, this handles all the module present
+class MasterOfModules
+{
+    public:
+        MasterOfModules()=default;
+        virtual ~MasterOfModules()
+        {
+            modules.clear();
         }
     protected:
-        ///protected ctor, empy obj creation is not allowed
-        Module(){}
-        ros::NodeHandle nh;
-        ros::Rate spin_rate;
-        std::unique_ptr<ros::CallbackQueue> queue_ptr;
-        std::shared_ptr<Storage> storage;
-};
+        std::unordered_map<std::string,MasterOfModules::anyPtr> modules;
 
+};
 /**\brief Class MasterNode
  * {:Brief Base Class implementing a ROS master node that uses Modules with
  * separate threads.}
 */
-class MasterNode
+class MasterNode : public BaseCommon
 {
     public:
+        ///empty obj creation is not allowed
+        MasterNode ()=delete;
         ///Only this ctor is allowed
-        MasterNode(std::string ns, ros::Rate rate)
+        MasterNode(std::string ns, ros::Rate &rate)
         {
             nh = ros::NodeHandle(ns);
+            name_space = ns;
             storage.reset(new Storage);
             spin_rate = rate;
         }
@@ -124,20 +161,10 @@ class MasterNode
         ///Shut it down
         virtual void reset()
         {
-            modules.clear();
+            // modules.clear();
             storage.reset(new Storage);
         }
     protected:
-        ///Constructor protected, empty obj creation is not allowed
-        MasterNode (){}
-        ///Node Handle
-        ros::NodeHandle nh;
-        /// desired spin rate
-        ros::Rate spin_rate;
-        /// Shared pointer to Storage Class
-        std::shared_ptr<Storage> storage;
-        //Container of modules this class uses
-        std::vector<Module> modules;
         ///Check Modules Method
         virtual void checkModules()=0;
 };
