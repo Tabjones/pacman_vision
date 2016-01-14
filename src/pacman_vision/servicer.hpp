72dc1f0b2d585b51dd9f248617c66ecb3c60780d
Federico Spinelli <fspinelli@gmail.com>
2016-01-14 09:34:42 +0100

    Update Servicer

--- src/pacman_vision/servicer.hpp
+++ src/pacman_vision/servicer.hpp
@@ -1,8 +1,8 @@
 #ifndef _SERVICER_HPP_
 #define _SERVICER_HPP_
 
-#include <common/dynamic_modules.hpp>
 #include <common/common_ros.h>
+#include <common/common_std.h>
 // ROS services
 #include <pacman_vision_comm/estimate.h>
 #include <pacman_vision_comm/stop_track.h>
@@ -13,44 +13,45 @@
 #include <pacman_vision_comm/start_modeler.h>
 #include <pacman_vision_comm/stop_modeler.h>
 
+#include <thread>
+
 
 namespace pacv
 {
-class Servicer: public Module<Servicer>
+class Servicer
 {
-    friend class Module<Servicer>;
     public:
         Servicer()=delete;
-        Servicer(const ros::NodeHandle n, const std::string ns, const Storage::Ptr stor):
-            Module<Servicer>(n,ns,stor), busy(false)
-        {}
+        Servicer(const ros::NodeHandle n, const std::string ns): busy(false),
+                name_space(ns)
+        {
+            father_nh = std::make_shared<ros::NodeHandle>(n);
+        }
         typedef std::shared_ptr<Servicer> Ptr;
         inline bool isBusy() const
         {
             return busy;
         }
+        template <typename Service>
+        bool spawn(Service &srv, std::string topic)
+        {
+            nh = std::make_shared<ros::NodeHandle>(*father_nh, name_space);
+            client = nh.serviceClient<Service>(topic);
+            caller
+        }
     private:
         bool busy;
+        std::shared_ptr<ros::NodeHandle> father_nh, nh;
+        std::string name_space;
         ros::ServiceClient client;
+        std::thread caller;
         //init with ros param
         void init()
         {
-            if(!nh){
-                ROS_ERROR("[Servicer::%s]\tNode Handle not initialized, Module must call spawn() first",__func__);
-                return;
-            }
-            client = nh.serviceClient<pacman_vision_comm::estimate>("/pacman_vsion/estimator/estimate");
         }
-        //deinit to free memory
-        void deInit()
+        template <typename Service>
+        void call(Service &srv, std::string topic)
         {
-        }
-        //spin once
-        void spinOnce()
-        {}
-        void call()
-        {
-            pacman_vision_comm::estimate srv;
             busy = true;
             if (client.call(srv))
                 //success
